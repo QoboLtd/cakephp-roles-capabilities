ffb8db26d4732570bcb03110c32ca49d56126d4e
George Constantinou <g.constantinou@qobo.biz>
2016-02-04 19:04:59 +0200

    baked roles Table and Entity (task #805)

---
+++ src/Model/Table/RolesTable.php
@@ -0,0 +1,60 @@
+<?php
+namespace RolesCapabilities\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+use RolesCapabilities\Model\Entity\Role;
+
+/**
+ * Roles Model
+ *
+ * @property \Cake\ORM\Association\BelongsToMany $Capabilities
+ */
+class RolesTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('roles');
+        $this->displayField('name');
+        $this->primaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsToMany('Capabilities', [
+            'foreignKey' => 'role_id',
+            'targetForeignKey' => 'capability_id',
+            'joinTable' => 'capabilities_roles',
+            'className' => 'RolesCapabilities.Capabilities'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->add('id', 'valid', ['rule' => 'uuid'])
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('name', 'create')
+            ->notEmpty('name');
+
+        return $validator;
+    }
+}
---
+++ tests/Fixture/RolesFixture.php
@@ -0,0 +1,47 @@
+<?php
+namespace RolesCapabilities\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * RolesFixture
+ *
+ */
+class RolesFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'uuid', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        'name' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'utf8_general_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'id' => '7db8a390-d058-4e9d-a07c-6c7928979043',
+            'name' => 'Lorem ipsum dolor sit amet',
+            'created' => '2016-02-04 17:00:01',
+            'modified' => '2016-02-04 17:00:01'
+        ],
+    ];
+}
