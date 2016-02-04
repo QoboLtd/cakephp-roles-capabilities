9e0c194a7b1d44567dcde4505849446c6f556db0
George Constantinou <g.constantinou@qobo.biz>
2016-02-04 19:39:08 +0200

    baked capabilities controller (task #805)

---
+++ tests/TestCase/Controller/CapabilitiesControllerTest.php
@@ -0,0 +1,71 @@
+<?php
+namespace RolesCapabilities\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestCase;
+use RolesCapabilities\Controller\CapabilitiesController;
+
+/**
+ * RolesCapabilities\Controller\CapabilitiesController Test Case
+ */
+class CapabilitiesControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.roles_capabilities.capabilities'
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     */
+    public function testView()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     */
+    public function testAdd()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     */
+    public function testEdit()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
