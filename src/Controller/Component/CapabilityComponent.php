80fea58917eb2e1ec38910b58f7502688d214e37
George Constantinou <g.constantinou@qobo.biz>
2016-02-08 13:15:35 +0200

    added method for retrieving all controllers names (task #805)

--- src/Controller/Component/CapabilityComponent.php
+++ src/Controller/Component/CapabilityComponent.php
@@ -7,6 +7,27 @@ use Cake\ORM\TableRegistry;
 class CapabilityComponent extends Component
 {
     /**
+     * Method that returns all controller names.
+     * @param  bool  $includePlugins flag for including plugin controllers
+     * @return array                 controller names
+     */
+    protected function _getAllControllers($includePlugins = true)
+    {
+        $controllers = $this->_getDirControllers(APP . 'Controller' . DS);
+
+        if (true === $includePlugins) {
+            $plugins = \Cake\Core\Plugin::loaded();
+            foreach ($plugins as $plugin) {
+                // plugin path
+                $path = \Cake\Core\Plugin::path($plugin) . 'src' . DS . 'Controller' . DS;
+                $controllers = array_merge($controllers, $this->_getDirControllers($path, $plugin));
+            }
+        }
+
+        return $controllers;
+    }
+
+    /**
      * Method that retrieves controller names
      * found on the provided directory path.
      * @param  string $path   directory path
