77d62717b5af120e34d47d2b39e11a0a53aa0fe9
George Constantinou <g.constantinou@qobo.biz>
2016-02-10 16:03:05 +0200

    added method for converting controller name to human readable label (task #805)

--- src/View/Cell/CapabilityCell.php
+++ src/View/Cell/CapabilityCell.php
@@ -25,4 +25,25 @@ class CapabilityCell extends Cell
     public function display()
     {
     }
+
+    /**
+     * Method that converts Controller
+     * full name to human readable label
+     * @param  string $name Controller full name
+     * @return void
+     */
+    public function groupName($name)
+    {
+        $parts = array_map(
+            function ($n) {
+                return str_replace('Controller', '', $n);
+            },
+            explode('\\', $name)
+        );
+        $parts = array_filter($parts);
+
+        $name = implode(' ', $parts);
+
+        $this->set('groupName', $name);
+    }
 }
