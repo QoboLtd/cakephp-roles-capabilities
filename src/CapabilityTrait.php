268aff3a3bcd5487e4b5f6c4df45bb2eea5570b2
George Constantinou <g.constantinou@qobo.biz>
2016-05-03 18:02:02 +0300

    move hasAccess specific logic to Capability Trait (task #1923)

--- src/CapabilityTrait.php
+++ src/CapabilityTrait.php
@@ -1,6 +1,7 @@
 <?php
 namespace RolesCapabilities;
 
+use Cake\Event\Event;
 use ReflectionClass;
 use ReflectionMethod;
 
@@ -48,6 +49,54 @@ trait CapabilityTrait
         }
 
         return $result;
+    }
+
+    /**
+     * Check if current user has access to perform action.
+     *
+     * @param  Event  $event Event object
+     * @return void
+     * @throws Cake\Network\Exception\ForbiddenException
+     * @todo                 this needs re-thinking
+     */
+    protected function _checkAccess(Event $event)
+    {
+        $requestParams = $event->subject()->request->params;
+        $plugin = is_null($requestParams['plugin']) ? 'App' : $requestParams['plugin'];
+        $controllerName = $plugin . '\\Controller\\' . $event->subject()->name . 'Controller';
+        $capability = 'cap__' . $controllerName . '__' . $requestParams['action'];
+        $allCapabilities = $this->getCapabilities($controllerName);
+        $capExists = false;
+        foreach ($allCapabilities as $cap) {
+            if ($cap->getName() === $capability) {
+                $capExists = true;
+                break;
+            }
+        }
+
+        $hasAccess = false;
+        if ($capExists) {
+            if ($this->Capability->hasAccess($capability)) {
+                $hasAccess = true;
+            } else {
+                $hasAccess = false;
+            }
+        } else {
+            /*
+            if capability does not exist user is allowed access
+             */
+            $hasAccess = true;
+        }
+
+        /*
+        superuser has access everywhere
+         */
+        if ($this->Auth->user('is_superuser')) {
+            $hasAccess = true;
+        }
+        if (!$hasAccess) {
+            throw new ForbiddenException();
+        }
     }
 
     protected static function _getCakeControllerActions()
