a2ddede2c91b3dd28b68bd7b67b3732adc9bcca6
Michael Stepanov <michael@stepanoff.org>
2017-03-02 10:57:20 +0200

    Class with common methods

---
+++ src/Access/Utils.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace RolesCapabilities\Access;
+
+use Cake\Core\App;
+
+/**
+ *  Utils class with common methos for Capabilities
+ *
+ *
+ */
+class Utils 
+{
+    /**
+     * Returns Controller's class name namespaced.
+     *
+     * @param array $url array of URL parameters.
+     * @return string
+     */
+    public static function getControllerFullName(array $url)
+    {
+        $result = null;
+
+        if (empty($url['controller'])) {
+            return $result;
+        }
+
+        $class = $url['controller'];
+        if (!empty($url['plugin'])) {
+            $class = $url['plugin'] . '.' . $class;
+        }
+        $result = App::className($class . 'Controller', 'Controller');
+
+        return $result;
+    }
+
+}
