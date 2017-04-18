f06b4380ad660123f237d6d8ffdf746735481d62
Michael Stepanov <michael@stepanoff.org>
2017-04-18 15:40:17 +0300

    Loading new event (#task 3636)

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,8 +2,10 @@
 use Cake\Core\Configure;
 use Cake\Event\EventManager;
 use RolesCapabilities\Event\ModelBeforeFindEventsListener;
+use RolesCapabilities\Event\AddPersonalPermissionsListener;
 
 EventManager::instance()->on(new ModelBeforeFindEventsListener());
+EventManager::instance()->on(new AddPersonalPermissionsListener());
 
 // load default plugin config
 Configure::load('RolesCapabilities.roles_capabilities');
