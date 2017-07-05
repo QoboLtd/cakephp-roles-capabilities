914a7d486397f1983ab51328deda98fa0cc9a80c
George Constantinou <georgec20001@gmail.com>
2017-07-05 12:24:22 +0300

    Remove permissions listener (task #4123)

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,10 +1,8 @@
 <?php
 use Cake\Core\Configure;
 use Cake\Event\EventManager;
-use RolesCapabilities\Event\AddPermissionsListener;
 use RolesCapabilities\Event\ModelBeforeFindEventsListener;
 
-EventManager::instance()->on(new AddPermissionsListener());
 EventManager::instance()->on(new ModelBeforeFindEventsListener());
 
 // load default plugin config
