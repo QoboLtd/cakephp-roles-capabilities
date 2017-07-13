d0a1bf394b1d1112d0488d927005825876e6b99b
Michael Stepanov <michael@stepanoff.org>
2017-07-13 17:48:44 +0300

    Added checking parent modules acess - task #4178

--- src/Access/CapabilitiesAccess.php
+++ src/Access/CapabilitiesAccess.php
@@ -5,6 +5,7 @@ namespace RolesCapabilities\Access;
 use Cake\Core\App;
 use Cake\ORM\TableRegistry;
 use Cake\Utility\Inflector;
+use Qobo\Utils\ModuleConfig\ModuleConfig;
 use ReflectionClass;
 use ReflectionMethod;
 use RolesCapabilities\Access\Utils;
@@ -74,6 +75,46 @@ class CapabilitiesAccess extends AuthenticatedAccess
         $hasAccess = Utils::hasTypeAccess(Utils::getTypeOwner(), $actionCapabilities, $user, $url);
         if ($hasAccess) {
             return true;
+        }
+
+        $hasAccess = $this->_hasParentAccess($url, $user);
+        if ($hasAccess) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     *  _hasParentAccess method
+     *
+     * @param array $url    request URL
+     * @param array $user   user's session data
+     * @return bool         true or false
+     */
+    private function _hasParentAccess($url, $user)
+    {
+        $mc = new ModuleConfig(ModuleConfig::CONFIG_TYPE_MODULE, Inflector::camelize($url['controller']));
+        $moduleConfig = (array)json_decode(json_encode($mc->parse()), true);
+
+        $parents = $moduleConfig['table']['parent_modules'];
+        foreach ($parents as $parent) {
+            $parentUrl = $url;
+            $parentUrl['controller'] = $parent;
+
+            $controllerName = Utils::getControllerFullName($parentUrl);
+            $actionCapabilities = Utils::getCapabilities($controllerName, [$parentUrl['action']]);
+
+            $hasAccess = Utils::hasTypeAccess(Utils::getTypeOwner(), $actionCapabilities, $user, $parentUrl);
+            if ($hasAccess) {
+                return true;
+            }
+
+            // if user has no full access capabilities
+            $hasAccess = Utils::hasTypeAccess(Utils::getTypeOwner(), $actionCapabilities, $user, $parentUrl);
+            if ($hasAccess) {
+                return true;
+            }
         }
 
         return false;
