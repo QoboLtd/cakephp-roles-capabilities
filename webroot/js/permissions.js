b481b9aa1a17ddd6b6f164bf98a6639f6f3e9217
George Constantinou <georgec20001@gmail.com>
2017-07-20 11:11:20 +0300

    Remove console.log leftover (task #4178)

--- webroot/js/permissions.js
+++ webroot/js/permissions.js
@@ -1,8 +1,6 @@
 (function ($) {
     // Show popup with personal permissions
     $('.delete-permission-btn').click(function () {
-        console.log('Permissions ...');
-
         return false;
     });
 })(jQuery);
