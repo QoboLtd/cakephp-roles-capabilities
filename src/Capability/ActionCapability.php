1730b6aa597893ba688093e1a1366dc6a2efabba
George Constantinou <georgec20001@gmail.com>
2020-03-27 15:17:38 +0200

    Action capability class (task #4194)

---
+++ src/Capability/ActionCapability.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace RolesCapabilities\Capability;
+
+use Cake\Utility\Inflector;
+
+final class ActionCapability extends AbstractCapability
+{
+
+    /**
+     * Constructor method.
+     *
+     * @param string $resource Resource name
+     * @param string $operation Operation type
+     */
+    public function __construct(string $resource, string $operation)
+    {
+        $this->resource = $resource;
+        $this->operation = $operation;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName() : string
+    {
+        return sprintf('cap__%s__%s', $this->resource, $this->operation);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDescription() : string
+    {
+        // cameCaseMethod -> under_score -> Human Form -> lowercase
+        $action = strtolower(Inflector::humanize(Inflector::underscore($this->operation)));
+
+        switch ($action) {
+            case 'index':
+                $action = 'list';
+                break;
+            case 'info':
+            case 'changelog':
+                $action = 'view ' . $action;
+                break;
+        }
+
+        return sprintf('Allow %s', $action);
+    }
+}
