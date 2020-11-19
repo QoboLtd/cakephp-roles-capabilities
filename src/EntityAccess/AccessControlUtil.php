652203c2257d493e7408308e5c8e542f3fcfe55c
Nicos Panayides <nicosp@gmail.com>
2020-11-19 14:36:57 +0200

    Add convenience functions to AccessControlUtil (task #4194)

--- src/EntityAccess/AccessControlUtil.php
+++ src/EntityAccess/AccessControlUtil.php
@@ -6,4 +6,47 @@ namespace RolesCapabilities\EntityAccess;
 class AccessControlUtil
 {
     use AccessControlTrait;
+
+    /**
+     * @var ?SubjectInterface
+     */
+    private $subject;
+
+    /**
+     * @param ?SubjectInterface $subject The subject
+     */
+    public function __construct(?SubjectInterface $subject)
+    {
+        $this->subject = $subject;
+    }
+
+    /**
+     * Creates a new AccesControlUtil from the current context
+     *
+     * @return AccessControlUtil
+     */
+    public static function fromContext(): AccessControlUtil
+    {
+        $ctx = AuthorizationContextHolder::context();
+
+        if (empty($ctx)) {
+            $subject = null;
+        } else {
+            $subject = $ctx->subject();
+        }
+
+        return new AccessControlUtil($subject);
+    }
+
+    /**
+     * Checks whether the given operation is allowed.
+     * @param Table $table The table to check
+     * @param string $operation The operation
+     * @param ?string $entityId The entity
+     * @return bool
+     */
+    public function isAllowed(Table $table, string $operation, ?string $entityId): bool
+    {
+        return $this->isTableActionAuthorized($table, $operation, $entityId, $this->subject);
+    }
 }
