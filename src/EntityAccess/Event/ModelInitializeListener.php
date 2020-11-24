49e778dc4d4e44435ea981d8fb445d43e042a41b
Nicos Panayides <nicosp@gmail.com>
2020-11-24 10:30:54 +0200

    Added model initialize listener to allow applications to add the behavior to plugin tables easily (task #4194)

---
+++ src/EntityAccess/Event/ModelInitializeListener.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace RolesCapabilities\EntityAccess\Event;
+
+use Cake\Core\Configure;
+use Cake\Event\Event;
+use Cake\Event\EventListenerInterface;
+use Cake\ORM\Table;
+
+class ModelInitializeListener implements EventListenerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function implementedEvents()
+    {
+        return [
+            'Model.initialize' => 'initializeEvent',
+        ];
+    }
+
+    /**
+     * Handler for initiaze event.
+     *
+     * @param Event $event The event to handle
+     * @return void
+     */
+    public function initializeEvent(Event $event): void
+    {
+        $table = $event->getSubject();
+        if (!($table instanceof Table)) {
+            return;
+        }
+
+        if ($table->hasBehavior('Authorized')) {
+            return;
+        }
+
+        $key = str_replace('.', '_', $table->getRegistryAlias());
+
+        $config = Configure::read('RolesCapabilities.tables.' . $key);
+        if ($config === null) {
+            return;
+        }
+
+        $table->addBehavior('RolesCapabilities.Authorized', $config);
+    }
+}
