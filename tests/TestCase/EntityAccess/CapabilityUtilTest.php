08135dca8ec032f860f89dce3b52e68e3b09cbbc
Nicos Panayides <nicosp@gmail.com>
2020-11-25 17:46:54 +0200

    Remove unused fixtures from CapabilitiesUtil test (task #4194)

--- tests/TestCase/EntityAccess/CapabilityUtilTest.php
+++ tests/TestCase/EntityAccess/CapabilityUtilTest.php
@@ -9,32 +9,6 @@ use RolesCapabilities\EntityAccess\CapabilitiesUtil;
 
 class CapabilityUtilTest extends TestCase
 {
-    /**
-     * Fixtures
-     *
-     * @var array
-     */
-    public $fixtures = [
-        'plugin.RolesCapabilities.ExtendedCapabilities',
-        'plugin.RolesCapabilities.Groups',
-        'plugin.RolesCapabilities.GroupsRoles',
-        'plugin.RolesCapabilities.GroupsUsers',
-        'plugin.RolesCapabilities.Permissions',
-        'plugin.RolesCapabilities.Roles',
-        'plugin.RolesCapabilities.Users',
-    ];
-
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
-
-    public function tearDown(): void
-    {
-        TableRegistry::clear();
-        parent::tearDown();
-    }
-
     public function testListTables(): void
     {
         $tables = CapabilitiesUtil::getTables();
