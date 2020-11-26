232b1d9a98b5269e6b18b0037ee4c19785742510
Nicos Panayides <nicosp@gmail.com>
2020-11-26 10:08:56 +0200

    Added more test cases for CapabilitiesUtil (task #4194)

--- tests/TestCase/EntityAccess/CapabilityUtilTest.php
+++ tests/TestCase/EntityAccess/CapabilityUtilTest.php
@@ -3,11 +3,40 @@ declare(strict_types=1);
 
 namespace RolesCapabilities\Test\TestCase\EntityAccess;
 
+use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
 use RolesCapabilities\EntityAccess\CapabilitiesUtil;
+use RolesCapabilities\EntityAccess\Operation;
 
 class CapabilityUtilTest extends TestCase
 {
+    /**
+     * @var ?\Cake\ORM\Table
+     */
+    private $Users;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->Users = TableRegistry::getTableLocator()->get('RolesCapabilities.Users');
+        $this->Users->addBehavior('RolesCapabilities.Authorized', [
+            'associations' => [
+                'Self' => [ 'association' => 'field', 'field' => 'id'],
+            ],
+            'capabilities' => [
+                ['operation' => Operation::VIEW, 'association' => 'Self'],
+            ],
+        ]);
+    }
+
+    public function tearDown(): void
+    {
+        TableRegistry::clear();
+        $this->Users = null;
+        parent::tearDown();
+    }
+
     public function testListTables(): void
     {
         $tables = CapabilitiesUtil::getTables();
@@ -15,5 +44,19 @@ class CapabilityUtilTest extends TestCase
         $this->assertContains('CakeDC/Users.Users', $tables, 'Table not found');
         $this->assertContains('Groups.Groups', $tables, 'Table not found');
         $this->assertContains('RolesCapabilities.Roles', $tables, 'Table not found');
+    }
+
+    public function testGetStaticCapabilities(): void
+    {
+        $capabilities = CapabilitiesUtil::getModelStaticCapabities($this->Users);
+
+        $this->assertCount(1, $capabilities, 'Table does not have exactly 1 capability');
+    }
+
+    public function testGetStaticAssociations(): void
+    {
+        $associations = CapabilitiesUtil::getModelCapabilityAssociations($this->Users);
+
+        $this->assertArrayHasKey('Self', $associations);
     }
 }
