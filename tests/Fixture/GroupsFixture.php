bbb9c5c2c338c30f6b743f92b2073d10c818127f
Nicos Panayides <nicosp@gmail.com>
2020-12-01 16:05:25 +0200

    Fix groups fixture (task #4194)

--- tests/Fixture/GroupsFixture.php
+++ tests/Fixture/GroupsFixture.php
@@ -22,9 +22,12 @@ class GroupsFixture extends TestFixture
     public $fields = [
         'id' => ['type' => 'uuid', 'null' => false],
         'name' => ['type' => 'string', 'length' => 255, 'null' => false],
+        'description' => ['type' => 'string', 'length' => 255, 'null' => false],
         'remote_group_id' => ['type' => 'string', 'length' => 255, 'null' => true],
         'created' => ['type' => 'datetime', 'null' => false],
         'modified' => ['type' => 'datetime', 'null' => false],
+        'deny_delete' => ['type' => 'boolean', 'null' => false],
+        'deny_edit' => ['type' => 'boolean', 'null' => false],
         'trashed' => ['type' => 'datetime', 'null' => true],
         '_constraints' => [
             'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
@@ -45,6 +48,9 @@ class GroupsFixture extends TestFixture
             'created' => '2016-02-04 11:12:29',
             'modified' => '2016-02-04 11:12:29',
             'trashed' => null,
+            'description' => 'Lorem ipsum dolor sit amet',
+            'deny_edit' => 0,
+            'deny_delete' => 0,
         ],
         [
             'id' => '969b9de4-07c7-4032-a9a0-0d075ca2c633',
@@ -53,6 +59,9 @@ class GroupsFixture extends TestFixture
             'created' => '2016-02-04 11:12:29',
             'modified' => '2016-02-04 11:12:29',
             'trashed' => null,
+            'description' => 'Test group',
+            'deny_edit' => 0,
+            'deny_delete' => 0,
         ],
         [
             'id' => '79628943-0016-4677-869a-e37728ff6564',
